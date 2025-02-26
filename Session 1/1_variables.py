9d04c8d92cb74318c3d87c9dfb5f556711d6a26b
Erfan Rajati Haghi <main.e.rajati@gmail.com>
2025-02-27 00:00:01 +0330

    update session 1

--- Session 1/1_variables.py
+++ Session 1/1_variables.py
@@ -1,29 +1,26 @@
+
+# Declaring variables
 a = 10
 b = 20
-
-# print(a)
 
 name = 'Erfan'
 message = "Hello, World!"
 
+# printing them
 print(a)
 print(b)
 print(name)
 print(message)
 
+# changing them
 name = "New value"
 print(name)
 
+# doing operations on them
 print(a + b)
 print(a - b)
 print(name + "Rajati")
 
+# getting user input
 user_input = input("Type your input here: ")
 
-# userIn = input()
-# print(userIn)
-
-# print(input())
-
-# print(hello)
-# Name Error
