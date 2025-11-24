49d93959dfe3742c4df60a536d3e51ad43ace974
Erfan Rajati Haghi <main.e.rajati@gmail.com>
2025-11-24 10:41:59 +0330

    add Session 05

---
+++ Session 05/for_loop.py
@@ -0,0 +1,25 @@
+# nums = [0, 2, 4, 6, 8]
+# for i in nums:
+#     print(i)
+
+# name = "Channel 1337"
+# for c in name:
+#     print(c)
+
+nums = range(10, 0, -1)
+
+# print(list(nums))
+
+
+
+
+# for i in range(10):
+#     print(i)
+
+
+
+for i in range(5):
+    for j in range(10):
+        print('#', end=' ')
+    print()
+
---
+++ Session 05/iterables.py
@@ -0,0 +1,82 @@
+# nums = [1, 2, 3, 4]
+
+# print(nums)
+
+# nums.append(10)
+
+# print(nums)
+
+# print(type(nums))
+
+
+# msg = 'Hello my name is erfan!'
+
+# msg = msg.replace(' ', '---')
+
+# print(msg)
+
+nums = [129, 4556, 432, 789, 1010]
+
+# print(len(nums))
+
+# print(nums[-2]) # index
+
+msg = 'Hello, World!'
+
+# print(msg[2:7])
+
+# i = 0
+# while i < len(msg):
+#     print(msg[i])
+#     i += 1
+
+
+msg = 'Hello, World!'
+sliced_msg = '' # 2:7
+
+# i = 2
+# while i < 7:
+#     sliced_msg += msg[i]
+#     i += 1
+
+# print(msg[2:7])
+# print(sliced_msg)
+
+# upper_msg = ''
+# i = 0
+# while i < len(msg):
+#     upper_msg += msg[i].lower()
+#     i += 1
+
+# print(msg)
+# print(upper_msg)
+
+
+nums1 = [1, 3, 5, 7]
+nums2 = [7, 8, 9, 10]
+
+nums1 += nums2
+
+# i = 0
+# while i < len(nums2):
+#     nums1.append(nums2[i])
+#     i += 1
+
+# print(nums1)
+
+
+nums = [1, 6, 3, 2, 8, 3, 0, 2, 3, 5, 6, 4, 10]
+
+# print(sorted(nums))
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+
+i = 0
+while i < len(matrix):
+    print(matrix[i])
+    i += 1
